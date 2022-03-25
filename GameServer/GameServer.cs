7425f269ec0f4e6756bbab2e2bd8be520bc03bd1
airdeng-Tang <1174141181@qq.com>
2022-03-25 13:57:40 +0800

    添加项目文件。

---
+++ GameServer/GameServer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Configuration;
+
+using System.Threading;
+
+using Network;
+using GameServer.Services;
+
+namespace GameServer
+{
+    class GameServer
+    {
+        Thread thread;
+        bool running = false;
+        NetService network;
+        //UserService user;
+
+        public bool Init()
+        {
+            //int Port = Properties.Settings.Default.ServerPort;
+            network = new NetService();
+            network.Init(8000);
+            //在类名后面加上:Singleton<>时则调用方法时不需要new仅需要在类名后接Instance(全局对象)接方法即可
+            //参考:https://zhidao.baidu.com/question/1510467911375124700.html
+            HelloWorldService.Instance.Init();//初始化服务 HelloWorldService
+            DBService.Instance.Init();
+            UserService.Instance.Init();
+            //user.Init();
+            thread = new Thread(new ThreadStart(this.Update));
+
+            return true;
+        }
+
+        public void Start()
+        {
+            network.Start();
+            HelloWorldService.Instance.Start();//启动服务 HelloWorldService
+            running = true;
+            thread.Start();
+        }
+
+
+        public void Stop()
+        {
+            
+            running = false;
+            thread.Join();
+            
+            network.Stop();
+        }
+
+        public void Update()
+        {
+            while (running)
+            {
+                Time.Tick();
+                Thread.Sleep(100);
+                //Console.WriteLine("{0} {1} {2} {3} {4}", Time.deltaTime, Time.frameCount, Time.ticks, Time.time, Time.realtimeSinceStartup);
+            }
+        }
+    }
+}
---
+++ GameServer/Services/HelloWorldService.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using Network;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Services
+{
+    public class HelloWorldService : Singleton<HelloWorldService>
+    {
+        public void Init()
+        {
+            //entities = new ExtremeWorldEntities();
+        }
+        public void Start()
+        {
+            //用(OnFirstTestRequest())方法处理 订阅消息(协议FirstTestRequest)  
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<FirstTestRequest>(this.OnFirstTestRequest);
+        }
+
+
+        //处理订阅的消息
+        private void OnFirstTestRequest(NetConnection<NetSession> sender, FirstTestRequest request)
+        {
+            //throw new NotImplementedException();
+            Log.InfoFormat("UserLoginRequest: Helloworld:{0} ", request.Helloworld);
+
+            //sender.SendData();//返回客户端信息方法
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
